a732e0794158cb2205225723c093de6da7d41b78
yangtk <531571494@qq.com>
2016-12-20 00:18:53 +0800

    add

--- js/main.js
+++ js/main.js
@@ -15,10 +15,16 @@ function toggle(obj,className){
         }
     }
 }
+function addZero(num){
+    return num<10?"0" + num:""+num;
+}
 
 //公共变量
 var oA = document.getElementById('audio1')  //mp3
 var aCtrls = document.querySelectorAll('.control span') //五个按钮
+var oNeedle = document.querySelector('.needle')
+var oDisk = document.querySelector('.disk')
+var oTotalTime = document.querySelector('.total-time')
 var onOff = true;
 
 var player = { // 播放器对象
@@ -34,16 +40,33 @@ player.play = function(){
         oA.pause();
     }
     onOff = !onOff
-    // oDisk.style.WebkitAnimationPlayState="running";
 }
 
+player.totalTime = function(){
+
+    var min = Math.floor(oA.duration/60);
+    var sec = Math.floor(oA.duration%60);
+    return addZero(min) +":"+addZero(sec);
+}
+
+player.init = function(){
+    oTotalTime.innerHTML = player.totalTime();
+}
+
+window.onload = function(){
+    player.init();
+}
 
 // -------按钮-------
+//播放按钮
 aCtrls[2].onclick = function(){
 
     player.play();
     toggle(this,'on');
+    toggle(oNeedle,'in');
+    toggle(oDisk,'rotate');    
 
 }
+
 
 
